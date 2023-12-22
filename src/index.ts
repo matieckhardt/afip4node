8c1065ab0ac91bb7d9214aa7622ec01a64db2709
matieckhardt <matias.eckhardt@gmail.com>
2023-12-22 11:52:02 -0300

    removing unused code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import { AfipAuth } from "./services/auth/AfipAuthWSFE";
-import { WsfeService } from "./services";
 import bodyParser from "body-parser";
 import afipRoutes from "./routes/afipRoutes";
 import cors from "cors"; // Import CORS module
@@ -14,13 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json());
 app.use(cors());
-
-const afipAuth = new AfipAuth(
-  "./src/certs/privateKey.pem",
-  "./src/certs/cert.pem",
-  false
-);
-const wsfeService = new WsfeService(afipAuth);
 
 app.use(morgan("dev")); // 'dev' format for development logging
 
